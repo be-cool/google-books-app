72b73bc6188d24fe4103f63dc9a50c318112b9a7
Ben Cooley <bcooley58@gmail.com>
2019-10-16 19:51:35 -0400

    updates

--- google-books-app/client/src/components/BookDetails/book-details.component.jsx
+++ google-books-app/client/src/components/BookDetails/book-details.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavItems from './../nav-items/nav-items.component';
 
 const BookDetails = (props) => {
-  const { title, authors, description, thumbnail , infoLink } = props.book;
+  const { title, authors, plot, thumbnail , infoLink } = props.book;
   const navItems = [
     {
       name: 'View',
@@ -16,14 +16,15 @@ const BookDetails = (props) => {
         <h2 className="book-title">{title}</h2>
         <NavItems book={JSON.stringify(props.book)} navItems={navItems} handleFunction={props.handleFunction} action={props.action} view/>
       </div>
+
       <div className="details-wrapper">
         <h3 className="book-authors">{authors}</h3>
         <div className="description-img-wrapper">
           <img className="book-image" src={thumbnail} alt={title} />
-           <p>{description}</p>
+           <p>{plot}</p>
         </div>
-        <a href={infoLink} target="_blank" rel="noopener noreferrer">Click here to buy</a>
       </div>
+      
     </div>
   )
 }
